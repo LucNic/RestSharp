e79170f38661fc937fdf4afdfeb4edadd71df437
LucNic <jlprosdocimo@gmail.com>
2017-11-15 17:39:30 -0300

    no message

--- RS_Tauro/Tauro Autopartes - Balsamo API.cs
+++ RS_Tauro/Tauro Autopartes - Balsamo API.cs
@@ -33,13 +33,10 @@ namespace RS_Tauro
 
                 {
 
-
                     list_request.AddParameter("application/x-www-form-urlencoded", "cliente=2222&pagina=" + pag.ToString() + "&apikey=f71ww547-ee22-2ftgd-987f-55557f0f3rr8f-CL-2222", ParameterType.RequestBody);
                     IRestResponse list_response = list_client.Execute(list_request);
                     Text = list_response.Content.ToString();
-                    //Text = Text.Replace(",", Environment.NewLine);
 
-                    //string _text = Text.ToString();
                     string[] lineas = Text.Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
                     List<string> _list = new List<string>();
                     int count = lineas.Count();
@@ -53,22 +50,26 @@ namespace RS_Tauro
 
                     }
                     string _error = "";
+
+                    var client = new RestClient("http://api.balsamo.com.ar:9086/v1/articulos");
+                    var request = new RestRequest(Method.POST);
+                    request.AddHeader("cache-control", "no-cache");
+                    request.AddHeader("content-type", "application/x-www-form-urlencoded");
+                    
                     foreach (string _codArt in _list)
                     {
-                        string exp_codart = _codArt.Substring(1, _codArt.Length - 2);
+                        string exp_codart = _codArt.Substring(1, _codArt.Length - 3);
                         using (System.IO.StreamWriter exp_file = new System.IO.StreamWriter(@"C:\temp\Balsamo.txt"))
                         {
-                            var client = new RestClient("http://api.balsamo.com.ar:9086/v1/articulos");
-                            var request = new RestRequest(Method.POST);
-
-                            //"cliente": "2222",
-                            //"api-key": "f71ww547-ee22-2ftgd-987f-55557f0f3rr8f-CL-2222"
-                            request.AddHeader("cache-control", "no-cache");
-                            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-                            request.AddParameter("application/x-www-form-urlencoded", "cliente=2222&articulo=" + exp_codart + "&apikey=f71ww547-ee22-2ftgd-987f-55557f0f3rr8f-CL-2222", ParameterType.RequestBody);
-                            IRestResponse response = client.Execute(request);
-                            exp_file.WriteLine(response.Content.ToString() + Environment.NewLine);
-                            _error = response.Content.ToString();
+                            int art = 1;
+                            if (art < 2)
+                            {
+                                request.AddParameter("application/x-www-form-urlencoded", "cliente=2222&articulo=" + exp_codart + "&apikey=f71ww547-ee22-2ftgd-987f-55557f0f3rr8f-CL-2222", ParameterType.RequestBody);
+                                IRestResponse response = client.Execute(request);
+                                exp_file.WriteLine(response.Content.ToString() + Environment.NewLine);
+                                _error = response.Content.ToString();
+                                art++;
+                            }
                         }
 
                     }
