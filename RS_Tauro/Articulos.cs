ffaf22aa1b37da2fc038e331ae6cdaf172515d64
LucNic <jlprosdocimo@gmail.com>
2017-11-26 19:22:04 -0300

    no message

---
+++ RS_Tauro/Articulos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS_Tauro
+{
+    public class Articulos
+    {
+        string _descripcion { get; set; }
+        string _artcodigo { get; set; }
+        string _original { get; set; }
+        string _rubro { get; set; }
+        int _costo { get; set; }
+    }
+}
--- RS_Tauro/Tauro Autopartes - Balsamo API.cs
+++ RS_Tauro/Tauro Autopartes - Balsamo API.cs
@@ -9,6 +9,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RestSharp;
+using Newtonsoft;
+using System.IO;
+
 
 
 namespace RS_Tauro
@@ -20,74 +23,41 @@ namespace RS_Tauro
             InitializeComponent();
         }
 
-        public void Listado_articulos()
+        public void _leerlistado()
         {
-            var list_client = new RestClient("http://api.balsamo.com.ar:9086/v1/articulos/listado");
-            var list_request = new RestRequest(Method.POST);
-            list_request.AddHeader("cache-control", "no-cache");
-            list_request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            string _path = @"c:\temp\balsamo.txt";
+            var client = new RestClient("http://api.balsamo.com.ar:9086/v1/articulos");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            
+            string[] _leerTXT = File.ReadAllLines(_path);
 
-            int pag = 1;
-            try {
-                while (pag < 2)
-
+            using (System.IO.StreamWriter exp_file = new System.IO.StreamWriter(@"C:\temp\BalsamoWeb.txt"))
+            {
+                foreach (string _art in _leerTXT)
                 {
+                    string _artParam = _art.Trim();
 
-                    list_request.AddParameter("application/x-www-form-urlencoded", "cliente=2222&pagina=" + pag.ToString() + "&apikey=f71ww547-ee22-2ftgd-987f-55557f0f3rr8f-CL-2222", ParameterType.RequestBody);
-                    IRestResponse list_response = list_client.Execute(list_request);
-                    Text = list_response.Content.ToString();
+                    request.AddParameter("application/x-www-form-urlencoded", "cliente=2222&articulo=" + _artParam + "&apikey=f71ww547-ee22-2ftgd-987f-55557f0f3rr8f-CL-2222", ParameterType.RequestBody);
+                    IRestResponse response = client.Execute(request);
 
-                    string[] lineas = Text.Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
-                    List<string> _list = new List<string>();
-                    int count = lineas.Count();
-                    for (int i = 0; i < count; i++)
-                    {
-                        if (i >= 2 & (i % 2 == 0))
-                        {
-                            _list.Add(lineas[i]);
+                    //dynamic _artweb = Newtonsoft.Json.JsonConvert.DeserializeObject(_txt);
+                    //var _desc = _artweb.
+                    Articulos  _artweb = Newtonsoft.Json.JsonConvert.DeserializeObject<Articulos>(response.Content.ToString());
 
-                        }
-
-                    }
-                    string _error = "";
-
-                    var client = new RestClient("http://api.balsamo.com.ar:9086/v1/articulos");
-                    var request = new RestRequest(Method.POST);
-                    request.AddHeader("cache-control", "no-cache");
-                    request.AddHeader("content-type", "application/x-www-form-urlencoded");
                     
-                    foreach (string _codArt in _list)
-                    {
-                        string exp_codart = _codArt.Substring(1, _codArt.Length - 3);
-                        using (System.IO.StreamWriter exp_file = new System.IO.StreamWriter(@"C:\temp\Balsamo.txt"))
-                        {
-                            int art = 1;
-                            if (art < 2)
-                            {
-                                request.AddParameter("application/x-www-form-urlencoded", "cliente=2222&articulo=" + exp_codart + "&apikey=f71ww547-ee22-2ftgd-987f-55557f0f3rr8f-CL-2222", ParameterType.RequestBody);
-                                IRestResponse response = client.Execute(request);
-                                exp_file.WriteLine(response.Content.ToString() + Environment.NewLine);
-                                _error = response.Content.ToString();
-                                art++;
-                            }
-                        }
 
-                    }
+                    exp_file.WriteLine(_artweb.ToString() + Environment.NewLine);
 
-                    pag++;
-                    lblMensaje.Text = _error;
+
                 }
-
-                
-                MessageBox.Show("Archivo completo", "Mensaje", MessageBoxButtons.OK,MessageBoxIcon.Information);
             }
-            catch (Exception e)
-            { MessageBox.Show(e.ToString()); }
+        }
 
-}        
         private void btnDescargar_Click(object sender, EventArgs e)
         {
-            Listado_articulos();
+            _leerlistado();            
         }
     }
 }
